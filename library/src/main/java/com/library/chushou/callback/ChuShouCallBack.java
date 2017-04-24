8dd7634a3d378fd41c36b9a09026e95685c04365
xiangcheng <liuxm20@lenovo.com>
2017-04-24 15:34:36 +0800

    还原换手操作的代码

--- library/src/main/java/com/library/chushou/callback/ChuShouCallBack.java
+++ library/src/main/java/com/library/chushou/callback/ChuShouCallBack.java
@@ -38,9 +38,6 @@ public class ChuShouCallBack extends ItemTouchHelper.SimpleCallback {
 
     private OnSwipedListener onSwipedListener;
 
-    private float fingerY;
-    private float baseFingerY;
-
     /**
      * 下一个要展示的view，如果是向下拉的话，就是上一个view，如果是往上拉的话就是下一个view
      */
@@ -81,9 +78,6 @@ public class ChuShouCallBack extends ItemTouchHelper.SimpleCallback {
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
         Log.d(TAG, "onSwiped");
         lastDy = 0;
-        isChangeFinger = false;
-        baseFingerY = 0;
-//        canPull = false;
         refreshData(viewHolder);
         if (onSwipedListener != null) {
             onSwipedListener.onSwiped(pullDown);
@@ -119,7 +113,6 @@ public class ChuShouCallBack extends ItemTouchHelper.SimpleCallback {
     //add  2017/4/17，为了解决换手指的bug，当松手指的时候用到的值动画
     ValueAnimator valueAnimator;
 
-    boolean isChangeFinger;
 
     /**
      * 监听recyclerView切换item的事件
@@ -134,10 +127,10 @@ public class ChuShouCallBack extends ItemTouchHelper.SimpleCallback {
         Log.d(TAG, "dy:" + dY + ",actionState:" + actionState + ",isCurrentlyActive:" + isCurrentlyActive);
 //        Log.d(TAG, "lastDy:" + lastDy);
         //add  2017/4/17，此时换了手指再去按住nextView，如果是下拉时：lastDy > 0 && dY <= 0，如果是上拉时：lastDy < 0 && dY >= 0
+        Log.d(TAG, "height * getSwipeThreshold(viewHolder):" + height * getSwipeThreshold(viewHolder));
         if (lastDy > 0 && dY <= 0 || lastDy < 0 && dY >= 0) {
+//        if (lastDy > 0 && dY <= height * getSwipeThreshold(viewHolder) || lastDy < 0 && dY >= 0) {
             //这个是当松手时isCurrentlyActive=false
-            isChangeFinger = true;
-            baseFingerY = lastDy;
             if (!isCurrentlyActive) {
                 if (valueAnimator == null) {
                     //从松手一瞬间，从lastDy的位置到0
@@ -163,8 +156,6 @@ public class ChuShouCallBack extends ItemTouchHelper.SimpleCallback {
                         @Override
                         public void onAnimationEnd(Animator animation) {
                             lastDy = 0;
-                            isChangeFinger = false;
-                            baseFingerY = 0;
                             valueAnimator = null;
                         }
                     });
@@ -174,48 +165,30 @@ public class ChuShouCallBack extends ItemTouchHelper.SimpleCallback {
 
         } else {
             //该种情况就是没有换手指的情况
-            if (!isChangeFinger) {
-                pullSome(recyclerView, viewHolder, dY);
-            } else {
-                Log.d(TAG, "换了手指，继续往下拉或往上拉的情况");
-                //如果是在换手指情况下呢
-                fingerY = baseFingerY + dY;
-                if (fingerY >= height) {
-                    fingerY = height;
+            float percent = Math.abs(dY / height);
+            float scaleAlpha = (float) (1.0 - percent * 1.0);
+            viewHolder.itemView.setAlpha(scaleAlpha);
+            ((ViewGroup) viewHolder.itemView).getChildAt(0).setScaleX(scaleAlpha);
+            ((ViewGroup) viewHolder.itemView).getChildAt(0).setScaleY(scaleAlpha);
+            //往下拉
+            if (dY > 0) {
+                nextView = recyclerView.getChildAt(recyclerView.getChildCount() - 1);
+                View childAt = ((ViewGroup) nextView).getChildAt(0);
+                if (childAt instanceof SlideRecyclerView) {
+                    SlideRecyclerView sl = (SlideRecyclerView) childAt;
+                    if (sl.getScrollY() == 0) {
+                        sl.pullNextScroll();
+                    }
                 }
-                if (fingerY <= -height) {
-                    fingerY = -height;
-                }
-                Log.d(TAG, "fingerY:" + fingerY);
-                pullSome(recyclerView, viewHolder, fingerY);
+                nextView.setTranslationY(dY);
+                pullDown = true;
+                lastDy = dY;
+            } else if (dY < 0) {
+                nextView = recyclerView.getChildAt(1);
+                pullDown = false;
+                nextView.setTranslationY(dY);
+                lastDy = dY;
             }
-        }
-    }
-
-    private void pullSome(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dY) {
-        float percent = Math.abs(dY / height);
-        float scaleAlpha = (float) (1.0 - percent * 1.0);
-        viewHolder.itemView.setAlpha(scaleAlpha);
-        ((ViewGroup) viewHolder.itemView).getChildAt(0).setScaleX(scaleAlpha);
-        ((ViewGroup) viewHolder.itemView).getChildAt(0).setScaleY(scaleAlpha);
-        //往下拉
-        if (dY > 0) {
-            nextView = recyclerView.getChildAt(recyclerView.getChildCount() - 1);
-            View childAt = ((ViewGroup) nextView).getChildAt(0);
-            if (childAt instanceof SlideRecyclerView) {
-                SlideRecyclerView sl = (SlideRecyclerView) childAt;
-                if (sl.getScrollY() == 0) {
-                    sl.pullNextScroll();
-                }
-            }
-            nextView.setTranslationY(dY);
-            pullDown = true;
-            lastDy = dY;
-        } else if (dY < 0) {
-            nextView = recyclerView.getChildAt(1);
-            pullDown = false;
-            nextView.setTranslationY(dY);
-            lastDy = dY;
         }
     }
 
